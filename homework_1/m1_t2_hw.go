4cd28d8fac7fbb60f97e6abbfcf1c3ea7a8c1690
k.kuznetsov <k.kuznetsov@gge.ru>
2023-10-23 15:06:38 +0300

    Add Homework_1_2

--- homework_1/m1_t2_hw.go
+++ homework_1/m1_t2_hw.go
@@ -1,6 +1,6 @@
 /*
 Задача № 2. Получить реверсную запись трехзначного числа
-Пример: 
+Пример:
 вход: 346, выход: 643
 вход: 100, выход: 001
 */
@@ -10,19 +10,19 @@ package main
 import "fmt"
 
 func main() {
-	var inputNumber int;
+	var inputNumber int
 
 	fmt.Println("Введите трехзначное число (100 - 999)")
-    fmt.Scanf("%d\n", &inputNumber)
+	fmt.Scanf("%d\n", &inputNumber)
 
-    if inputNumber < 100 || inputNumber > 999 {
-        fmt.Println("Некорретное значение, допустимый диапазон значений (100 - 999)")
-        return
-    }
+	if inputNumber < 100 || inputNumber > 999 {
+		fmt.Println("Некорретное значение, допустимый диапазон значений (100 - 999)")
+		return
+	}
 
 	var hundreds int = inputNumber / 100
-	var tens int = (inputNumber - hundreds * 100) / 10
-	var units int = inputNumber - hundreds * 100 - tens * 10
+	var tens int = (inputNumber - hundreds*100) / 10
+	var units int = inputNumber - hundreds*100 - tens*10
 
 	fmt.Printf("Result = %v%v%v\n", units, tens, hundreds)
 
