70f202e3f1ffbdb6d38d10887d08cbb61de368a2
k.kuznetsov <k.kuznetsov@gge.ru>
2023-10-23 15:42:53 +0300

    Add Homework_1_4

--- homework_1/m2_t2_hw.go
+++ homework_1/m2_t2_hw.go
@@ -4,3 +4,36 @@
 Вход: 1221  Выход: 1221 - палиндром
 Вход: 1234  Выход: 1234 - не палиндром
 */
+
+package main
+
+import "fmt"
+
+func main() {
+	var inputNumber int
+	var isNumberPolindrom bool = false
+	
+	fmt.Println("Введите четырехзначное число (1000 - 9999)")
+	fmt.Scanf("%d\n", &inputNumber)
+
+	if inputNumber < 1000 || inputNumber > 9999 {
+		fmt.Println("Некорретное значение, допустимый диапазон значений (100 - 999)")
+		return
+	}
+
+	var thousands int = inputNumber / 1000
+	var hundreds int = (inputNumber - thousands * 1000) / 100
+	var tens int = (inputNumber - thousands * 1000 - hundreds * 100) / 10
+	var units int = inputNumber - thousands * 1000 - hundreds * 100 - tens * 10
+
+	if (thousands * 10 + hundreds == units * 10 + tens){
+		isNumberPolindrom = true
+	}
+
+	if isNumberPolindrom {
+		fmt.Printf("Number %d is Polindrom\n", inputNumber)
+	} else {
+		fmt.Printf("Number %d is not Polindrom\n", inputNumber)
+	}
+	
+}
